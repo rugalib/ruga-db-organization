61c25e5c96d32bb1d36e52bed84646ac6f758969
Roland Rusch <roland@rusch.lu>
2022-05-25 09:01:21 +0200

    #4 Change comment

--- src/Organization.php
+++ src/Organization.php
@@ -6,7 +6,7 @@ namespace Ruga\Organization;
 
 /**
  * Implements the organization entity.
- * Can be used in rrlib3 projects to add a general organization entity.
+ * Can be used in ruga projects to add a general organization entity.
  *
  * @author   Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
  */
